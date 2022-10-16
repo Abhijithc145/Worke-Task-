06613346c5a6d01a25bb3db330b9ea56177ff9b0
Abhijithc145 <abhijithabhipgmr145@example.com>
2022-10-16 20:13:48 +0530

    To setup postgres in the project

--- contactcenter/settings.py
+++ contactcenter/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'contactcenter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "contact",
+        'USER': "postgres",
+        'PASSWORD': "postgres",
+        'HOST': "localhost"
     }
 }
 
